5b3e5537f6a94910d6dbe4c6325524568c86cb5c
Tm-A-T <like.chima@gmail.com>
2018-06-06 15:36:25 +0300

    Исправил Ванюту. Вроде работает

--- Interpreter.py
+++ Interpreter.py
@@ -8,9 +8,23 @@
 
 
 import math
-import Color
 import sys
 
+RED = '\033[1;31m'
+BLUE  = '\033[1;34m'
+CYAN  = '\033[1;36m'
+GREEN = '\033[0;32m'
+RESET = '\033[0;0m'
+BOLD    = '\033[;1m'
+REVERSE = '\033[;7m'
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
 
 #Check if param can be number
 #Проверить, может ли параметр быть числом
@@ -25,9 +39,9 @@ def isNum(x):
 #Сообщения об ошибках
 def ERROR(q):
     print()
-    print(Color.RED + "ERROR: " + q)
+    print(RED + "ERROR: " + q)
     print()
-    print(Color.RED + "[Exiting program]")
+    print(RED + "[Exiting program]")
     print()
     exit(0);
 
